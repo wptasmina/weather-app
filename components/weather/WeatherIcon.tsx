847e77ca139561b6d7b53b91d841c32cb4f35a98
wptasmina <wptasmina@gmail.com>
2025-06-21 20:22:29 +0600

    WeatherIcon add

--- components/weather/WeatherIcon.tsx
+++ components/weather/WeatherIcon.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client"
 
-export default function WeatherIcon() {
-  return (
-    <div>
-      
-    </div>
-  )
+import { Sun, Cloud, CloudRain, CloudSnow } from "lucide-react"
+
+const WeatherIcon = ({ icon, size = 24 }: { icon: string; size?: number }) => {
+  const iconMap = {
+    sunny: Sun,
+    "partly-cloudy": Cloud,
+    cloudy: Cloud,
+    rainy: CloudRain,
+    snowy: CloudSnow,
+  }
+
+  const IconComponent = iconMap[icon as keyof typeof iconMap] || Sun
+  return <IconComponent size={size} className="text-blue-400" />
 }
+
+export default WeatherIcon
