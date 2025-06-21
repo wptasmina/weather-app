443529963366b58668253f74cd3e68c1a3499f83
wptasmina <wptasmina@gmail.com>
2025-06-21 20:37:21 +0600

    WeatherLoading add

--- components/weather/WeatherLoading.tsx
+++ components/weather/WeatherLoading.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client"
+
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function WeatherLoading() {
   return (
-    <div>
-      
+    <div className="space-y-6">
+      <div className="text-center">
+        <Skeleton className="h-8 w-64 mx-auto bg-white/20" />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {[...Array(4)].map((_, i) => (
+          <Skeleton key={i} className="h-64 bg-white/20 rounded-xl" />
+        ))}
+      </div>
     </div>
   )
 }
