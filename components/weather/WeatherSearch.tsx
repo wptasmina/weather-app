26b2f967e2c82be64a22b3af02ca476eae2c3e62
wptasmina <wptasmina@gmail.com>
2025-06-21 20:24:55 +0600

    remove useState

--- components/weather/WeatherSearch.tsx
+++ components/weather/WeatherSearch.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+
 import { Search, Globe } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
